d6b9bc305e25f4f48a08d5fb23204b6f4b8427a4
EgorX2000 <ebkrivoshapkin@yandex.ru>
2024-10-06 15:38:49 +0300

    lab_2

---
+++ 2/src/Seven.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+
+class Seven {
+   public:
+    Seven();
+    Seven(unsigned int decimal_value);
+    Seven(const Seven& obj) noexcept;
+
+    Seven add(const Seven& obj) const;
+    Seven substract(const Seven& obj) const;
+
+    bool equals(const Seven& obj) const;
+    bool greater_than(const Seven& obj) const;
+    bool less_than(const Seven& obj) const;
+
+    void print();
+
+    ~Seven() noexcept;
+
+   private:
+    Seven(unsigned int decimal_value, unsigned int len);
+    void strip();
+
+   private:
+    unsigned int len;
+    unsigned char* array;
+};
